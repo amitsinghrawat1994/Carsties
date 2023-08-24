bf45b60baa14dd1cb669291b4a9bc02537b995a4
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-08-24 07:46:32 +0530

    37. Adding and configuring mass transit

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -15,6 +16,14 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<AuctionServiceHttpClient>()
 .AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
