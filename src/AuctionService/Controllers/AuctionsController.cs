19cdd0a35933bbbea409d1ea430de94b598933a9
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-08-25 08:33:43 +0530

    42. Adding a message outbox

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -59,11 +59,13 @@ public class AuctionsController : ControllerBase
         auction.Seller = "test";
         _context.Auctions.Add(auction);
 
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
-        var newAuction = _mapper.Map<AuctionDto>(auction);
-
-        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+        // var newAuction = _mapper.Map<AuctionDto>(auction);
+        // await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
         if (!result)
         {
--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService;
 
@@ -8,6 +9,15 @@ public class AuctionDbContext : DbContext
     {
     }
 
-    public DbSet<Auction> Auctions{ get; set; }
-    
+    public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
+
 }
