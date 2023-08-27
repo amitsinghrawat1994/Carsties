831935b84df03b5e455e00101a47f5a106b45720
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-08-27 13:04:03 +0530

    45. Challenge: Adding the update and delete consumers

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -92,6 +92,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Mileage = auctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = auctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish<AuctionUpdated>(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (!result)
@@ -113,6 +115,11 @@ public class AuctionsController : ControllerBase
         }
 
         _context.Auctions.Remove(auction);
+
+        await _publishEndpoint.Publish<AuctionDeleted>(new
+        {
+            Id = auction.Id.ToString()
+        });
 
         var result = await _context.SaveChangesAsync() > 0;
 
