8c24b1483860206a622857b680d189dfee559b5c
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-12-28 13:30:02 +0530

    88. Styling the auction cards

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { json } from 'stream/consumers';
+import AuctionCard from './AuctionCard';
 
 async function getData() {
-    const res = await fetch('http:localhost:6001/search');
+    const res = await fetch('http:localhost:6001/search?pageSize=10');
 
     if (!res.ok) {
         throw new Error('failed to fetch data');
@@ -15,8 +16,10 @@ export default async function Listings() {
     var data = await getData();
 
     return (
-        <div>{
-            JSON.stringify(data, null, 2)
-        }</div>
+        <div className='grid grid-cols-4 gap-6'>
+            {data && data.results.map((auction: any) => (
+                <AuctionCard auction={auction} key={auction.id} />
+            ))}
+        </div>
     )
 }
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -3,7 +3,8 @@ import Listings from "./auctions/Listings";
 export default function Home() {
   return (
     <div>
-      <h3 className='text-3xl font-semibold'>
+      {/* text-3xl  */}
+      <h3 className='font-semibold'>
         <Listings />
       </h3>
     </div>
