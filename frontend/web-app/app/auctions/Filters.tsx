4b6097247c36702e6d1caa998ffbba7b5681e447
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-12-30 19:38:27 +0530

    101. Adding the filtering functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -2,7 +2,8 @@ import { Button } from 'flowbite-react';
 import React from 'react'
 import { userParamStore } from '../hooks/userParamsStore';
 import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
-import { BsFillStopCircleFill } from 'react-icons/bs';
+import { BsFillStopCircleFill, BsStopwatchFill } from 'react-icons/bs';
+import { GiFinishLine, GiFlame } from 'react-icons/gi'
 
 const pageSizeButtons = [4, 8, 12];
 const orderButton = [
@@ -23,13 +24,47 @@ const orderButton = [
     },
 ];
 
+const filterButton = [
+    {
+        label: 'Live Auctions',
+        icon: GiFlame,
+        value: 'live'
+    },
+    {
+        label: 'Ending < 6 hours',
+        icon: GiFinishLine,
+        value: 'endingSoon'
+    },
+    {
+        label: 'Completed',
+        icon: BsStopwatchFill,
+        value: 'finished'
+    },
+];
+
 export default function Filters() {
     const pageSize = userParamStore(state => state.pageSize);
     const setParams = userParamStore(state => state.setParams);
     const orderBy = userParamStore(state => state.orderBy);
+    const filterBy = userParamStore(state => state.filterBy);
 
     return (
         <div className='flex justify-between items-center mb-4'>
+            <div>
+                <span className='uppercase text-sm text-gray-500 mr-2'>Filter by</span>
+                <Button.Group>
+                    {filterButton.map(({ label, icon: Icon, value }) => (
+                        <Button
+                            key={value}
+                            onClick={() => setParams({ filterBy: value })}
+                            color={`${filterBy === value ? 'red' : 'gray'}`}
+                        >
+                            <Icon className='mr-3 h-4 w-4' />
+                            {label}
+                        </Button>
+                    ))}
+                </Button.Group>
+            </div>
             <div>
                 <span className='uppercase text-sm text-gray-500 mr-2'>Order by</span>
                 <Button.Group>
