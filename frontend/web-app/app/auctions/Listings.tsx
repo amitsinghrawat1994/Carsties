5856e4ccaf929ef5c36a181a8e2cfc6197081148
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2023-12-30 19:56:14 +0530

    102. Adding a component to display when zero results

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { userParamStore } from '../hooks/userParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default function Listings() {
     const [data, setData] = useState<PagedResult<Auction>>();
@@ -37,17 +38,24 @@ export default function Listings() {
 
     if (!data) return (<div><h3 >Loading....</h3></div>)
 
+    if (data.totalCount === 0) return <EmptyFilter showReset />
+
     return (
         <>
             <Filters />
-            <div className='grid grid-cols-4 gap-6'>
-                {data.results.map((auction) => (
-                    <AuctionCard auction={auction} key={auction.id} />
-                ))}
-            </div>
-            <div className='flex justify-center mt-4'>
-                <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
-            </div>
+            {data.totalCount === 0 ? (<EmptyFilter showReset />) : (
+                <>
+                    <div className='grid grid-cols-4 gap-6'>
+                        {data.results.map((auction) => (
+                            <AuctionCard auction={auction} key={auction.id} />
+                        ))}
+                    </div>
+                    <div className='flex justify-center mt-4'>
+                        <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
+                    </div>
+                </>
+            )}
+
         </>
     )
 }
