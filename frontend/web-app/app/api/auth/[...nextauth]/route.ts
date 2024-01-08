97b58f05ff6bc63345cf3218cef59af7674c82cb
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-01-08 21:00:20 +0530

    113. Getting the access token to use to authenticate to our resource server

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -21,6 +21,10 @@ export const authOptions: NextAuthOptions = {
                 token.username = profile.username
             }
 
+            if (account) {
+                token.access_token = account.access_token;
+            }
+
             return token;
         },
         async session({ session, token }) {
--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Heading from '../components/Heading';
-import { getSession } from '../actions/authActions';
+import { getSession, getTokenWorkaround } from '../actions/authActions';
 import AuthText from './AuthText';
 
 export default async function session() {
     const session = await getSession();
+    const token = await getTokenWorkaround();
 
     return (
         <div>
@@ -16,7 +17,10 @@ export default async function session() {
             <div className='mt-4'>
                 <AuthText />
             </div>
-
+            <div className='bg-green-200 border-2 border-blue-500 mt-4'>
+                <h3 className='text-lg'>Session data</h3>
+                <pre className='overflow-auto'>{JSON.stringify(token, null, 2)}</pre>
+            </div>
         </div>
     )
 }
