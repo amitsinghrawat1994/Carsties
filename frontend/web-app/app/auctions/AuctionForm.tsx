27b78d1450c3c143160e007664add4417ecf2351
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-01-20 13:37:47 +0530

    127. Adding the edit auction page

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -5,31 +5,51 @@ import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
 import DateInput from '../components/DateInput';
-import { createAuction } from '../actions/auctionActions';
-import { useRouter } from 'next/navigation';
+import { createAuction, updateAuction } from '../actions/auctionActions';
+import { usePathname, useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
+import { Auction } from '@/types';
 
-export default function AuctionForm() {
+type Props = {
+    auction?: Auction
+}
+
+export default function AuctionForm({ auction }: Props) {
     const router = useRouter();
+    const pathName = usePathname();
 
-    const { control, handleSubmit, setFocus,
+    const { control, handleSubmit, setFocus, reset,
         formState: { isSubmitting, isValid, isDirty, errors } } = useForm({
             mode: 'onTouched'
         });
 
     useEffect(() => {
+        if (auction) {
+            const { make, model, color, mileage, year } = auction;
+            reset({ make, model, color, mileage, year })
+        }
         setFocus('make');
     }, [setFocus]);
 
     async function onSubmit(data: FieldValues) {
         try {
-            const res = await createAuction(data);
+            let id = '';
+            let res;
+            if (pathName === '/auctions/create') {
+                res = await createAuction(data);
+                id = res.id;
+            } else {
+                if (auction) {
+                    res = await updateAuction(data, auction.id);
+                    id = auction.id;
+                }
+            }
 
             if (res.error) {
                 throw res.error;
             }
 
-            router.push(`/auctions/details/${res.id}`)
+            router.push(`/auctions/details/${id}`)
         }
         catch (error: any) {
             console.log(error);
@@ -53,21 +73,25 @@ export default function AuctionForm() {
                     rules={{ required: 'Mileage is required' }} />
             </div>
 
-            <Input label='Image UEL' name='imageUrl' control={control}
-                rules={{ required: 'Image url is required' }} />
+            {pathName === '/auctions/create' &&
+                <>
+                    <Input label='Image UEL' name='imageUrl' control={control}
+                        rules={{ required: 'Image url is required' }} />
 
-            <div className='grid grid-cols-2 gap-3'>
-                <Input label='Reserve Price (enter 0 if no reserve)' type='number'
-                    name='reservedPrice' control={control}
-                    rules={{ required: 'Reserve price is required' }} />
-                <DateInput
-                    label='Auction end date/time'
-                    name='auctionEnd'
-                    control={control} type='date'
-                    dateFormat='dd MMM yyyy h:mm a'
-                    showTimeSelect
-                    rules={{ required: 'Auction end date is required' }} />
-            </div>
+                    <div className='grid grid-cols-2 gap-3'>
+                        <Input label='Reserve Price (enter 0 if no reserve)' type='number'
+                            name='reservedPrice' control={control}
+                            rules={{ required: 'Reserve price is required' }} />
+                        <DateInput
+                            label='Auction end date/time'
+                            name='auctionEnd'
+                            control={control} type='date'
+                            dateFormat='dd MMM yyyy h:mm a'
+                            showTimeSelect
+                            rules={{ required: 'Auction end date is required' }} />
+                    </div>
+                </>
+            }
 
             <div className='flex justify-between'>
                 <Button outline color='gray'>Cancel</Button>
