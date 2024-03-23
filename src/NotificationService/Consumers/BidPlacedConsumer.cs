2745b1ce58e460ce0997ab18f289ba09eec61124
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-03-23 17:41:57 +0530

    151. Adding the Consumers

---
+++ src/NotificationService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NotificationService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public BidPlacedConsumer(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+
+    public Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> auction finished message received");
+
+        return _hubContext.Clients.All.SendAsync("BidPlaced", context.Message);
+    }
+}
