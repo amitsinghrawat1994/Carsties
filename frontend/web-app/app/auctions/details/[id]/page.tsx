149dc64efba0b453c43b2f12e6c701cc49c51a9a
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-04-20 21:28:26 +0530

    162. Creating a bid form

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { getDetailedViewData } from '@/app/actions/auctionActions'
+import { getBidsForAuctions, getDetailedViewData } from '@/app/actions/auctionActions'
 import Heading from '@/app/components/Heading';
 import React from 'react'
 import CountdownTimer from '../../CountdownTimer';
@@ -7,10 +7,13 @@ import DetailedSpecs from './DetailsSpaces';
 import { getCurrentUser } from '@/app/actions/authActions';
 import EditButton from './EditButton';
 import DeleteButton from './DeleteButton';
+import BidItem from './BidItem';
+import BidList from './BidList';
 
 export default async function Details({ params }: { params: { id: string } }) {
     const data = await getDetailedViewData(params.id);
     const user = await getCurrentUser();
+    // const bids = await getBidsForAuctions(params.id);
 
     return (
         <div>
@@ -33,9 +36,7 @@ export default async function Details({ params }: { params: { id: string } }) {
                 <div className='w-full bg-gray-200 aspect-h-10 aspect-w-16 rounded-lg overflow-hidden'>
                     <CarImages imageUrl={data.imageUrl} />
                 </div>
-                <div className='border-2 rounded-lg p-2 bg-gray-100'>
-                    <Heading title='Bids' />
-                </div>
+                <BidList user={user} auction={data} />
             </div>
             <div className='mt-3 grid grid-cols-1 rounded-lg'>
                 <DetailedSpecs auction={data} />
