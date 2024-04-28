ed17bf23920713341d2b59519ab5e28d5c17a858
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-04-28 11:29:05 +0530

    166. Adding the new bid to SignalR

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -28,13 +28,13 @@ export default function SignalRProvider({ children }: Props) {
         if (connection) {
             connection.start()
                 .then(() => {
-                    console.log('Connected to notification hub');
-
                     connection.on('BidPlaced', (bid: Bid) => {
                         console.log('Bid placed event received');
                         if (bid.bidStatus.includes('Accepted')) {
                             setCurrentPrice(bid.auctionId, bid.amount);
                         }
+
+                        addBid(bid);
                     })
                 }).catch(error => console.log(error));
         }
