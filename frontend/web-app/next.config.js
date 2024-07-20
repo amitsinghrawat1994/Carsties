2dcaa337744dc9cb140728237a623461e3fd67e5
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2024-07-20 14:32:47 +0530

    174. Creating the Dockerfile for nextjs

--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //     serverActions: true
-    // },
+    experimental: {
+        serverActions: true
+    },
     images: {
         domains: [
             "cdn.pixabay.com"
         ]
-    }
+    },
+    output: "standalone"
 }
 
 module.exports = nextConfig
